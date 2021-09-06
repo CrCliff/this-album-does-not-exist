abd97d2c8b19e6874b78e690202227d340e2a7dd
CrCliff <crcliff@comcast.net>
2021-09-06 16:48:36 -0400

    Update training script

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DATA_DIR="data/album_covers_512"
-DATE=$(date "+%Y_%m_%d_%H_%M_%S")
+DATA_DIR="./data/album_covers_512"
+DATE=$(date "+_%Y_%m_%d_%H_%M_%S")
 
-#lightweight_gan \
-#  --data "$DATA_DIR" \
-#  --name "$DATE" \
+lightweight_gan \
+  --data "$DATA_DIR" \
+  --name "main" \
+  --image-size 512 \
+  --batch-size 8 \
+  --gradient-accumulate-every 4 \
+  --num-train-steps 20000 \
+  --disc-output-size 5 \
+  --amp
 #  --show-progress \
-#  --batch-size 16 \
-#  --gradient-accumulate-every 4 \
-#  --num-train-steps 2 \
-#  --image-size 512 \
-#  --amp \
-#  --disc-output-size 5
